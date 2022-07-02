0e365061c5a2d3b0e414e6f0e2ace5e3bb1c11dd
tomjes03 <sonofdson@gmail.com>
2022-07-02 14:19:19 -0700

    My 9th python task commit message

---
+++ 0x00-python-hello_world/8-concar_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "python is an interpreted, interactive, 0bject-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + [107:112] + str[:6]
+print(str)
