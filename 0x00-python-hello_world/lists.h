92d325a3317aacf0774fa7db46665f9c857ad116
tomjes03 <sonofdson@gmail.com>
2022-07-02 15:06:26 -0700

    My 12th python task commit message

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -13,8 +13,8 @@
  *        */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+    int n;
+    	        struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,4 +23,3 @@ void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-Footer
