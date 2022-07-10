97dd9060b66f5891759350d177cb6b701ad8919d
tomjes03 <sonofdson@gmail.com>
2022-07-10 05:48:14 -0700

    My 1st python task commit message

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     copy = []
     for i in range(len(my_list)):
         if my_list[i] == search:
-            copy.append(replace)
+           copy.append(replace)
         else:
             copy.append(my_list[i])
-            return copy
+    return copy
