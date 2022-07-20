cc626013ed2cf56c2615dc715ec14f5a1bcc83fd
tomjes03 <sonofdson@gmail.com>
2022-07-20 16:11:38 -0700

    My python classes task commint message

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
@@ -8,12 +7,11 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self._size = size
+    Args:
+        size (int): The size of the new square.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
