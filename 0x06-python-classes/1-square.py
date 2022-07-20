5cc93d42fddf85fda4f49b7e1c842bdb66729c96
tomjes03 <sonofdson@gmail.com>
2022-07-20 05:13:00 -0700

    My python classes task commit message

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,9 +6,9 @@
 class Square:
     """Represent a square."""
 
-        def __init__(self, size):
-            """Initialize a new Square.
-            Args:
-                size (int): The size of the new square.
-            """
-            self.__size = size
+    def __init__(self, size):
+        """Initialize a new Square.
+    Args:
+        size (int): The size of the new square.
+    """
+    self.__size = size
