c7e904fe9c35595f7d835f874ae8faf7779ed80e
tomjes03 <sonofdson@gmail.com>
2022-07-23 14:22:32 -0700

    My python classe task commit message

--- 102-square.py
+++ 102-square.py
@@ -22,7 +22,7 @@ class Square:
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
-                raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
