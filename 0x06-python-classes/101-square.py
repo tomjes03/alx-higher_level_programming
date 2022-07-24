3dd64a50abcadbd975d35749f928cb85c9ead1ad
tomjes03 <sonofdson@gmail.com>
2022-07-24 16:52:37 -0700

    My python classes task commit commit mesage

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -34,12 +34,16 @@ class Square:
 
     @position.setter
     def position(self, value):
+        """Must be a tuple of 2 positive integers,
+        otherwise raise a TypeError exception
+        """
+    self.__position = size_value
+
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     def area(self):
         """Return the current area of the square."""
@@ -57,13 +61,13 @@ class Square:
             [print("#", end="") for k in range(0, self.__size)]
             print("")
 
-            def __str__(self):
-                        """Define the print() representation of a Square."""
-                                if self.__size != 0:
-                                                [print("") for i in range(0, self.__position[1])]
-                                                        for i in range(0, self.__size):
-                                                                        [print(" ", end="") for j in range(0, self.__position[0])]
-                                                                                    [print("#", end="") for k in range(0, self.__size)]
-                                                                                                if i != self.__size - 1:
-                                                                                                                    print("")
-                                                                                                                            return ("")
+    def __str__(self):
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+        if i != self.__size - 1:
+            print("")
+        return ("")
