8b464a76049e91f1af595c5426591f504e5c4155
tomjes03 <sonofdson@gmail.com>
2022-07-26 08:06:13 -0700

    My python classes task commit message

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+""" Square module
+"""
 
 
-    class Square:
-        """Represent a square."""
+class Square:
+    """ Define a Square class
+    """
+    def __init__(self, size=0):
+        """ Intialize methonds that store the size of the square. """
+        self.__size = size
 
-        def __init__(self, size=0):
-            """Initialize a new Square.
-            Args:
-                size (int): The size of the new square.
-            """
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
