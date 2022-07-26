611ff4bba0f0db628e4e6a12282a423cc36e77ab
tomjes03 <sonofdson@gmail.com>
2022-07-26 08:25:04 -0700

    My python classes task commit message

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+""" Square module
+"""
 
 
 class Square:
-    """Represent a square."""
-
+    """ Define a Square class
+    """
     def __init__(self, size=0):
-        """Initialize a new square.
+        """ Intialize methonds that store the size of the square. """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
-
-     @property
+    @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """ property for method size """
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """ Setter for method size """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """ The methode area returns area of square """
+        return int(self.__size) * int(self.__size)
