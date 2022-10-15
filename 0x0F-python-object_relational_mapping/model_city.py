99f903bec2fd82277722c9a2440ddd6c4cf3735c
tomjes03 <sonofdsonofgod@yahoo.com>
2022-10-15 10:57:43 -0700

    My Python Mapping task commit message

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""Definition of the City class"""
-
-from sqlalchemy.ext.declarative import declarative_base
+"""
+    Python file similar to model_state.py named model_city.py
+    that contains the class definition of a City
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
-    """Class City"""
-
+    """ Class """
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
