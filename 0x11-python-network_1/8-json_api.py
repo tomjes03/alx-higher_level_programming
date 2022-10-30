947a857de7b2989ddf51c606449d3bf7f2a3d4d8
Thomas Jesus <sonofdson@gmail.com>
2022-10-30 15:29:51 -0700

    My Python Network task commit message

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-""" script that takes in a letter and sends a POST request
-    to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+    Script that takes in a letter and sends a POST request to
+    https://0.0.0.0:5000/search_user with the letter as a parameter
+    the letter is sent in variable q, otherwise q is empty
+    displays [<id>] <name> if repsonse is properly JSON formatted
 """
 import requests
-from sys import argv
-
-
-def json_api():
-    """ sends a POST request to the passed URL with a letter as a parameter """
-    url = "http://0.0.0.0:5000/search_user"
-    if len(argv) < 2:
-        values = {'q': ""}
-    else:
-        values = {'q': argv[1]}
-    r = requests.post(url, data=values)
-    try:
-        response = r.json()
-        if response == {}:
-            print("No result")
-        else:
-            print('[{}] {}'.format(response.get('id'), response.get('name')))
-    except:
-        print("Not a valid JSON")
+import sys
 
 
 if __name__ == "__main__":
-    json_api()
+    if len(sys.argv) > 1:
+        payload = {'q': sys.argv[1]}
+    else:
+        payload = {'q': ""}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        r_json = r.json()
+        if r_json == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_json['id'], r_json['name']))
+    except ValueError:
+        print("Not a valid JSON")
