c7e535ca77931dd645a690537601884b56ceb672
Thomas Jesus <sonofdson@gmail.com>
2022-10-30 15:22:34 -0700

    My Python Network task commit message

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""
+    Script that fetches https://intranet.htbtn.io/status
+"""
+
+import requests
 
 
 if __name__ == "__main__":
-    import requests
-
-    r = requests.get('https://intranet.hbtn.io/status')
-
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
