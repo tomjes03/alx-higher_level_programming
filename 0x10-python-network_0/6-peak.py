b810610b26c12b41f8e7315b0e09504943b3f5c2
tomjes03 <sonofdsonofgod@yahoo.com>
2022-12-13 08:51:55 -0800

    My Python Network task commit message

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm."""
 
-
 def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
